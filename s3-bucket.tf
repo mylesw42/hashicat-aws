eb1603b3e4664f75c16ecaf6833a1d481d253fcd
mylesw42 <mylesw42@gmail.com>
2023-08-25 16:59:22 +0000

    Added s3 bucket module

---
+++ s3-bucket.tf
@@ -0,0 +1,12 @@
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "s3-hashicat"
+
+  bucket_prefix = var.prefix
+  
+  versioning = {
+    enabled = true
+  }
+
+}
